67713da600fa09e35421d71f74af86f440026205
Tauookie <tauookie@gmail.com>
2026-08-23 18:52:40 +0300

    Added soundcannon trigger

---
+++ Triggers/SoundCannonTrigger.cs
@@ -0,0 +1,37 @@
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CUButt.Triggers
+{
+
+    [HarmonyPatch(typeof(Sound), "Play", new Type[] { typeof(string), typeof(Vector2), typeof(bool), typeof(bool), typeof(Transform), typeof(float), typeof(float), typeof(bool), typeof(bool)})]
+    public class SoundCannonPatch
+    {
+
+        static void Prefix(string clip)
+        {
+            if (clip == "tinnitus")
+            {
+                List<float> tinnitusPattern = PatternGenerator.CreateImpulse(new List<VibrationCheckPoint>
+                {
+                    new VibrationCheckPoint(0.0f, 1.0f, InterpolationType.Linear),
+                    new VibrationCheckPoint(3f, 1f, InterpolationType.Smooth),
+                    new VibrationCheckPoint(15f, 0.3f, InterpolationType.Linear)
+                });
+                VibrationManager.AddSpeedSequence(tinnitusPattern, 1);
+            }
+            if (clip == "sonarouchblocked")
+            {
+                List<float> tinnitusPattern = PatternGenerator.CreateImpulse(new List<VibrationCheckPoint>
+                {
+                    new VibrationCheckPoint(0.0f, 0.0f, InterpolationType.Smooth),
+                    new VibrationCheckPoint(0.5f, 0.35f, InterpolationType.Smooth),
+                    new VibrationCheckPoint(2f, 0f, InterpolationType.Linear)
+                });
+                VibrationManager.AddSpeedSequence(tinnitusPattern);
+            }
+        }
+    }
+}
